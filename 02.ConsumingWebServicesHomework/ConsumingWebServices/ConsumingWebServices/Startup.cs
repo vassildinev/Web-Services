d743e91fa847d4d00f7d3b3ff78c0fa4a61c62d7
V.Dinev <vassildinev@gmail.com>
2015-11-24 21:45:43 +0200

    Added Consuming Web Services Homework

---
+++ 02.ConsumingWebServicesHomework/ConsumingWebServices/ConsumingWebServices/Startup.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsumingWebServices
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public class Startup
+    {
+        static void Main()
+        {
+            var t = new Task(DownloadPageAsync);
+            t.Start();
+            Console.WriteLine("Downloading page...");
+            Console.ReadLine();
+        }
+
+        static async void DownloadPageAsync()
+        {
+            // ... Target page.
+            string page = "http://www.vesti.bg/rss";
+
+            // ... Use HttpClient.
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(page))
+            using (HttpContent content = response.Content)
+            {
+                // ... Read the string.
+                string result = await content.ReadAsStringAsync();
+
+                // ... Display the result.
+                if (result != null &&
+                result.Length >= 50)
+                {
+                    Console.WriteLine(result.Substring(0, 50) + "...");
+                }
+            }
+        }
+    }
+}
