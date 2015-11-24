67fcf59b23f4e66f953dcd525d4f8da4e7f785a0
V.Dinev <vassildinev@gmail.com>
2015-11-24 21:49:25 +0200

    Added Wcf Homework

---
+++ 04.WcfHomework/DateTimeUtilities/DateTimeUtilities.Web/DateTimeService.svc.cs
@@ -0,0 +1,31 @@
+﻿namespace DateTimeUtilities.Web
+{
+    using System;
+
+    public class DateTimeService : IDateTimeService
+    {
+        public string GetDayOfWeek(DateTime date)
+        {
+            string dayOfWeekInEnglish = date.DayOfWeek.ToString();
+            switch (dayOfWeekInEnglish)
+            {
+                case "Monday":
+                    return "Понеделник";
+                case "Tuesday":
+                    return "Вторник";
+                case "Wednesday":
+                    return "Сряда";
+                case "Thursday":
+                    return "Четвъртък";
+                case "Friday":
+                    return "Петък";
+                case "Saturday":
+                    return "Събота";
+                case "Sunday":
+                    return "Неделя";
+                default:
+                    return "Невалиден ден от седмицата!";
+            }
+        }
+    }
+}
---
+++ 04.WcfHomework/DateTimeUtilities/StringUtilities.Library/StringService.cs
@@ -0,0 +1,24 @@
+﻿namespace StringUtilities.Library
+{
+    public class StringService : IStringService
+    {
+        public int GetOccurrences(string template, string text)
+        {
+            int occurrences = 0;
+            int index = -1;
+            while (true)
+            {
+                index = text.IndexOf(template, index + 1);
+                if (index == -1)
+                {
+                    break;
+                }
+
+                occurrences += 1;
+
+            }
+
+            return occurrences;
+        }
+    }
+}
